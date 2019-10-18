dbb45357067f8c4e5ec3d16c82729bab077bd45d
Adel <htwr86@gmail.com>
2019-10-18 16:17:40 +0300

    add layouts and main.hbs

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos.js');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine); // registration engine by key 'hbs'
+app.set('view engine', 'hbs'); // using handle-bars defoult
+app.set('views', 'views'); // folder for views
+
+app.use(todoRoutes);
+
 async function start() {
   try {
-    await mongoose.connect('', {
-      useNewUrlParser: true,
-      useFindAndModify: false
-    });
+    await mongoose.connect(
+      'mongodb+srv://adel:1q2w3e4r@cluster0-mpyfv.mongodb.net/todos',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+      }
+    );
     app.listen(PORT, () => {
       console.log(`server started on port ${PORT} `);
     });
