056e6a4827da97aafec3508bd236cb8fe3d15e95
Adel <htwr86@gmail.com>
2019-10-18 17:21:17 +0300

    add toggle class active in navbar

--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,17 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, resp) => {
-  resp.render('index');
+  resp.render('index', {
+    title: 'Todos list',
+    isIndex: true
+  });
+});
+
+router.get('/create', (req, resp) => {
+  resp.render('create', {
+    title: 'Create todo',
+    isCreate: true
+  });
 });
 
 module.exports = router;
