ef7d85933994528d0829f784451d6ad04f104a74
Adel <htwr86@gmail.com>
2019-10-19 16:23:25 +0300

    add class active to navbar items

--- routes/todos.js
+++ routes/todos.js
@@ -3,12 +3,13 @@ const router = Router();
 
 router.get('/', (req, resp) =>
   resp.render('index', {
-    title: 'index-page'
+    title: 'index-page',
+    isIndex: true
   })
 );
 
 router.get('/create', (req, resp) => {
-  resp.render('create', { title: 'create-Page' });
+  resp.render('create', { title: 'create-Page', isCreate: true });
 });
 
 module.exports = router;
