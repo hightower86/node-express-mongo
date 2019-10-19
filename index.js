1a6f59cc37c2de3c520a757d3b84aae831bda2e3
Adel <htwr86@gmail.com>
2019-10-19 12:34:13 +0300

    create express server and mongoose connect

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const server = express();
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log('server started');
+async function start() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://adel:1q2w3e4r@cluster0-mpyfv.mongodb.net/test?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    );
+    server.listen(PORT, () => {
+      console.log('server started');
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+server.set('views', './views');
+server.set('view engine', 'pug');
+
+server.get('/', (req, resp) => {
+  resp.render('index');
 });
+
+start();
