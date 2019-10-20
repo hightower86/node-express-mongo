a8bd28a9b252bca0d2c194239be5d18a9d1bef61
Adel <htwr86@gmail.com>
2019-10-21 01:41:15 +0300

    problem with post request solved!

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const todoRoutes = require('./routes/todos');
-
-const server = express();
-
-server.use(todoRoutes);
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
+const server = express();
+
+server.set('views', './views');
+server.set('view engine', 'pug');
+
+server.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.json());
+server.use(todoRoutes);
+// server.use(bodyParser.urlencoded({ extended: true }));
+// server.use(bodyParser.json());
 
 async function start() {
   try {
     await mongoose.connect(
-      'mongodb+srv://adel:1q2w3e4r@cluster0-mpyfv.mongodb.net/todos',
+      'mongodb+srv://adel:1q2w3e4r@cluster0-mpyfv.mongodb.net/todos?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useFindAndModify: false,
@@ -22,15 +35,13 @@ async function start() {
       console.log('server started');
     });
   } catch (e) {
-    console.log(e);
+    console.log('#-#-#-#-#-#-#', e);
   }
 }
 
-server.set('views', './views');
-server.set('view engine', 'pug');
-
-// server.get('/', (req, resp) => {
-//   resp.render('index');
+// server.post('/create', function(req, res, next) {
+//   console.log('&&&&&&&&&&', req.body);
+//   res.json(req.body);
 // });
 
 start();
--- routes/todos.js
+++ routes/todos.js
@@ -1,15 +1,33 @@
 const { Router } = require('express');
 const router = Router();
+const Todo = require('../models/Todo');
 
-router.get('/', (req, resp) =>
+router.get('/', async (req, resp) => {
+  const todos = await Todo.find({});
+  //console.log(todos);
   resp.render('index', {
     title: 'index-page',
-    isIndex: true
-  })
-);
+    isIndex: true,
+    todos
+  });
+});
 
 router.get('/create', (req, resp) => {
+  //console.log('***********', req);
   resp.render('create', { title: 'create-Page', isCreate: true });
 });
 
+router.post('/create', async (req, res) => {
+  //console.log('***********', req);
+  const todo = new Todo({
+    title: req.body.title
+  });
+  //console.log('***********', todo);
+  await todo.save();
+  res.redirect('/');
+});
+// router.post('/create', function(req, res, next) {
+//   console.log('&&&&&&&&&&', req.body);
+//   res.json(req.body);
+// });
 module.exports = router;
